3d42255718207f77bf258cb2dce730b8747ba956
Valeri Gorobets <valeri.gorobets@thomsonreuters.com>
2019-06-03 09:37:07 +0300

    Update ui

--- src/Components/Transactions/index.js
+++ src/Components/Transactions/index.js
@@ -1,16 +1,26 @@
 import React, {Component} from 'react';
 import './styles.css'
-import web3 from '../../web3';
+// import web3 from '../../web3';
+import { getTransactionHistory } from '../../Services/TransactionsHistoryService';
 
 export default class Transactions extends Component {
   state = {
-    balance: 0,
+    balance: 10,
+    transactionHistory: [],
   };
   componentDidMount = async () => {
-    const balance = await web3.eth.getBalance(this.props.clientWalletAddress);
-    this.setState({
-        balance: web3.utils.fromWei(balance, 'ether')
-    });
+    this.reloadTransactions();
+    // setInterval(this.reloadTransactions, 5000);
+    // const balance = await web3.eth.getBalance(this.props.clientWalletAddress);
+    // this.setState({
+    //     balance: web3.utils.fromWei(balance, 'ether')
+    // });
+  };
+
+  reloadTransactions = async () => {
+      const transactionHistory = await getTransactionHistory(this.props.clientWalletAddress);
+      console.log(transactionHistory);
+      this.setState({transactionHistory});
   };
 
   render() {
@@ -20,7 +30,16 @@ export default class Transactions extends Component {
                 <h2>Balance: {this.state.balance} HBL</h2>
             </div>
             <div className="transactions-list">
-                <h2>{this.state.balance}</h2>
+                {this.state.transactionHistory.map(transaction => (
+                    <div className="card" key={transaction.hash}>
+                        <p>{transaction.timeStamp}</p>
+                        <div>
+                            <p>From : {transaction.from || "No address found"}</p>
+                            <p>To : {transaction.to || "No address found"}</p>
+                        </div>
+                        <p>{transaction.value}</p>
+                    </div>
+                ))}
             </div>
         </div>
     );

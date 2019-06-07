26eadd1115ae0facf90b0bf5003145ddf555a8ad
Valeri Gorobets <Valeri.Gorobets@thomsonreuters.com>
2019-06-07 22:25:29 +0300

    Add chart component

--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -2,17 +2,26 @@ import React, {Component} from 'react';
 import './styles.css';
 import ContentView from "../ContentView";
 import Transactions from "../Transactions";
+import Chart from "../Chart";
 
 export default class Main extends Component {
   state = {
     clientWalletAddress: '0xc9E4C7e8E2C9055492E3971dD380a359f4Ec4111',
+    chart: {
+        'Math': 1,
+        'Chemistry': 1,
+        'Hello Book': 3,
+    },
   };
 
   render() {
     return (
       <div className="container">
         <div className="analyticsData">
-            <Transactions clientWalletAddress = {this.state.clientWalletAddress}/>
+            <div className="verticalAlign">
+                <Transactions clientWalletAddress = {this.state.clientWalletAddress}/>
+                <Chart chart = {this.state.chart}/>
+            </div>
             <ContentView clientWalletAddress = {this.state.clientWalletAddress}/>
         </div>
       </div>
